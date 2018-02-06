127a926bc03c7aa8bacece71af301324b4f994c2
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-02-06 10:51:06 +0000

    Update symlink_raw_fastq.sh

--- management/symlink_raw_fastq.sh
+++ management/symlink_raw_fastq.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Create symbolic links to file, while renaming the link as follows:
+
 ## basename_1.fastq.gz -> basename.fastq.1.gz
 ## basename_2.fastq.gz -> basename.fastq.2.gz
 
@@ -9,5 +11,8 @@ do
 	basefilename=$(basename ${fullfilename})
 	echo "basefilename: ${basefilename}"
 	newbasename=$(echo ${basefilename} | sed -re 's|_(.)\.fastq\.gz|.fastq.\1.gz|')
-	echo "newbasename: ${newbasename}" 
+	echo "newbasename: ${newbasename}"
+	cmd="ln -s ${raw_fastq_dir}/${basefilename} ${link_dir}/${newbasename}"
+	echo "${cmd}"
+	eval "$cmd"
 done
