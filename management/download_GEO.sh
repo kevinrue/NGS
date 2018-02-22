614bdd6e01d0cebbb11b82aed9f1f368f32fa32d
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-02-22 15:45:30 +0000

    extract into subfolders named after the GSE id

--- management/download_GEO.sh
+++ management/download_GEO.sh
@@ -13,12 +13,21 @@ gse_list=(GSE12345 GSE67890) # dummy examples
 
 for gse_id in ${gse_list[@]}
 do
-	echo "gse_id: ${gse_id}"
-	range_sub=${gse_id:0:$((${#gse_id}-3))}
-	echo "range_sub: ${range_sub}"
-	ftp_link="ftp://ftp.ncbi.nlm.nih.gov/geo/series/${range_sub}nnn/${gse_id}/suppl/${gse_id}_RAW.tar"
-	echo "ftp_link: ${ftp_link}"
-	wget -nv "${ftp_link}"
+        echo "gse_id: ${gse_id}"
+        range_sub=${gse_id:0:$((${#gse_id}-3))}
+        echo "range_sub: ${range_sub}"
+        file_basename="${gse_id}_RAW.tar"
+        ftp_link="ftp://ftp.ncbi.nlm.nih.gov/geo/series/${range_sub}nnn/${gse_id}/suppl/${file_basename}"
+        echo "ftp_link: ${ftp_link}"
+        cmd="wget -nv ${ftp_link}"
+        echo "cmd: ${cmd}"
+        #eval $cmd                                                                                                                                                                             
+        cmd="mkdir -v ${gse_id}"
+        echo "cmd: ${cmd}"
+        eval $cmd
+        cmd="tar -xvC ${gse_id} -f ${file_basename}"
+        echo "cmd: ${cmd}"
+        eval $cmd
 done
 
 echo "Complete"
