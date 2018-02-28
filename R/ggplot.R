b056f3370181f40a449c755ef61ee2b7aceb1818
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2018-02-28 09:29:38 +0000

    roxygen documentation

--- R/ggplot.R
+++ R/ggplot.R
@@ -1,7 +1,13 @@
 
-# Oscillate Y nudge above and below data points
-# y: amount of Y nudge
-# length: count of data points to nudge
+#' Oscillate Y nudge above and below data points
+#'
+#' @param y Amount of Y nudge
+#' @param length Count of data points to nudge
+#'
+#' @return Vector of Y nudge alternated and balanced across 0.
+#' @export
+#'
+#' @examples
 nudge_balanced <- function(y, length){
   direction <- rep_len(c(-1,1), length.out=length)
   direction * y
